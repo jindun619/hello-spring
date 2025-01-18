5074663bfdf204609402eaf5da70dc4a297f6a0c
jindun619 <jindun619@naver.com>
2025-01-18 23:34:48 +0800

    spring tutorial complete

---
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,33 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.JPAMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SpringConfig {
+
+    private final EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JPAMemberRepository(em);
+    }
+}
